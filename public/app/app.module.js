b64a07ab3669ef7324a1d4d6f3a55b427532ae3b
Gene Lim <Gene Lim>
2016-04-03 23:32:34 +0800

    Image Compressed Blob + Justified

--- public/app/app.module.js
+++ public/app/app.module.js
@@ -1,1 +1,1 @@
-angular.module('app',['ui.router','ngResource','ngFileUpload']); 
+angular.module('app',['ui.router','ngResource','ngFileUpload','ahdin']); 
--- public/app/dashboard/image.js
+++ public/app/dashboard/image.js
@@ -2,36 +2,56 @@ angular
     .module('app')
     .controller('DashboardImageController', DashboardImageController)
 
-DashboardImageController.$inject = ['$stateParams', 'Upload'];
+DashboardImageController.$inject = ['$stateParams', 'Upload','Ahdin'];
 
-function DashboardImageController($stateParams, Upload  ){ 
+function DashboardImageController($stateParams, Upload ,Ahdin ){ 
     var vm = this;
     vm.upload = upload;
-    vm.image = '';
     componentHandler.upgradeAllRegistered();
     console.log($stateParams)
     // $("#gallery").justifiedGallery({
     //     rowHeight : 500,
     //     margins : 7}
     // );
-    
+    var count = 1;
+     
     function upload(files,path,index) {
         console.log(files)
         if (files && files.length) {
-           loadImage(
-                files[0],
-                function (img) {
-                    $("#gallery > a:nth-child(1)").html(img);
-                   console.log(vm.image)
-                   console.log(img)
-                   $("#gallery").justifiedGallery({
+        //    loadImage(
+        //         files[0],
+        //         function (img) {
+        //             // count ++;
+        //             console.log('ll')
+        //             img.removeAttribute("width");
+        //             img.removeAttribute("height");
+        //             $("#gallery > a:nth-child(1)").html(img);
+                  
+   
+        //         //    document.body.appendChild(img);
+        //         },
+        //         {maxHeight: 600} // Options
+        //     );
+        Ahdin.compress({
+        sourceFile: files[0],
+        maxHeight: 500,
+        outputFormat: 'png'
+      }).then(function(compressedBlob) {
+        console.log(compressedBlob)
+        var urlCreator = window.URL || window.webkitURL;
+        var i = urlCreator.createObjectURL( compressedBlob );
+        console.log(i)
+        var oImg=document.createElement("img");
+oImg.setAttribute('src', i);
+$("#gallery > a:nth-child(1)").html(oImg);
+setTimeout(function(){ 
+         $("#gallery").justifiedGallery({
         rowHeight : 500,
         margins : 7}
+        
     );
-                //    document.body.appendChild(img);
-                },
-                {maxHeight: 600} // Options
-            );
+    }, 1000);
+      });
             // Upload.upload({url: '/api/upload', file: files[0]}).success(function (data, status, headers, config) {
             //     console.log(data.path)
             // });
